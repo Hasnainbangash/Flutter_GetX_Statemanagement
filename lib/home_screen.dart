64c11da7cb16d96e5e665b05dd8d7be4b9209552
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-14 20:23:26 +0500

    Some bugs get fixed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,20 +11,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  int x = 0;
+  int counter = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    // Timer run after the 1 second
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      x++;
-      setState(() {
 
-      });
-    });
   }
 
   @override
@@ -34,25 +28,12 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text("GetX Tutorials"),
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Text(x.toString(), style: TextStyle(fontSize: 60),),
-          ),
-          Expanded(
-              child: ListView.builder(
-                  itemCount: 10000,
-                  itemBuilder: (context, index){
-                return ListTile(
-                  title: Text(index.toString()),
-                );
-              }),
-          ),
-        ],
+      body: Center(
+        child: Text(counter.toString(), style: TextStyle(fontSize: 60),),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: (){
-            x++;
+            counter++;
             setState(() {
 
             });
@@ -61,3 +42,65 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+// This is the code that tells us why we want the state management for our app
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//
+//   int x = 0;
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//
+//     // Timer run after the 1 second
+//     Timer.periodic(Duration(seconds: 1), (timer) {
+//       x++;
+//       setState(() {
+//
+//       });
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     print('rebuild');
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("GetX Tutorials"),
+//       ),
+//       body: Column(
+//         children: [
+//           Center(
+//             child: Text(x.toString(), style: TextStyle(fontSize: 60),),
+//           ),
+//           Expanded(
+//               child: ListView.builder(
+//                   itemCount: 10000,
+//                   itemBuilder: (context, index){
+//                 return ListTile(
+//                   title: Text(index.toString()),
+//                 );
+//               }),
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//           onPressed: (){
+//             x++;
+//             setState(() {
+//
+//             });
+//           }
+//       ),
+//     );
+//   }
+// }
